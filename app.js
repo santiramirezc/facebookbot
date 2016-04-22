1ace61a2996d6cece73968e1af6a7493ed51f954
Santiago Ramírez <santiramirezc@gmail.com>
2016-04-22 15:20:56 -0500

    Codigo para confirmar token de fb añadido

--- app.js
+++ app.js
@@ -56,5 +56,12 @@ app.use(function(err, req, res, next) {
   });
 });
 
+app.get('/', function (req, res) {
+  if (req.query['hub.verify_token'] === 'CAAOkHhWLZBL0BAD2ViYVXR5PhJrdFatLG306yirHopiQviUZCR0lQUBaJp0XM3U2TR6yo6r6THNVxknl36Vq4dTudQRUtD42ctp5SnoHMSW1kZAg1ZCOevpPnttAY3P0setfx0Nr0sCkjTQuZBP2ZBHPYm358TZBZAyOREcDiPLUFKZAtMhimHpnpMfsHPvo8mSUZD') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
+})
+
 
 module.exports = app;
