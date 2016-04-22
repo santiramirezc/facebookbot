8c17f690f6426464824c222c860169912eda29d8
Santiago Ramírez <santiramirezc@gmail.com>
2016-04-22 15:25:55 -0500

    Añadí /webhook/ a la url que facebook valida

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.use(function(err, req, res, next) {
   });
 });
 
-app.get('/', function (req, res) {
+app.get('/webhook/', function (req, res) {
   if (req.query['hub.verify_token'] === 'CAAOkHhWLZBL0BAD2ViYVXR5PhJrdFatLG306yirHopiQviUZCR0lQUBaJp0XM3U2TR6yo6r6THNVxknl36Vq4dTudQRUtD42ctp5SnoHMSW1kZAg1ZCOevpPnttAY3P0setfx0Nr0sCkjTQuZBP2ZBHPYm358TZBZAyOREcDiPLUFKZAtMhimHpnpMfsHPvo8mSUZD') {
     res.send(req.query['hub.challenge']);
   }
