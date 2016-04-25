88493bc8dcfbbc8917d78286be32048aa35b1ab2
Santiago Ramírez <santiramirezc@gmail.com>
2016-04-25 15:26:42 -0500

    Manejando postbacks

--- routes/webhook.js
+++ routes/webhook.js
@@ -5,26 +5,31 @@ var router = express.Router();
 var token = "CAAOkHhWLZBL0BAL4NcsGntcBul2S1w0ZBj5u9sNJ7JQoX8ZB18STaw66E4zSHL3R6vbAoWPXqxapuQjQ6Yni2ZAulupxrLpSZA5dbDVGh6hNuXfqcMneZBing1HvmWCmmOxOewaDWPJ98siMVlgDj8xVoLlXZB1tpDqHzUzD5GVOfPXFkbSkMVjNo67moPrTZCwZD";
 
 router.get('/', function(req, res, next) {
-  if (req.query['hub.verify_token'] === '<validation_token>') {
-    res.send(req.query['hub.challenge']);
-  }
-  res.send('Error, wrong validation token');
+    if (req.query['hub.verify_token'] === '<validation_token>') {
+        res.send(req.query['hub.challenge']);
+    }
+    res.send('Error, wrong validation token');
 });
 
 router.post('/', function (req, res) {
-  console.log("Llego un mensaje al webhook")
-  messaging_events = req.body.entry[0].messaging;
-  for (i = 0; i < messaging_events.length; i++) {
-    event = req.body.entry[0].messaging[i];
-    sender = event.sender.id;
-    if (event.message && event.message.text) {
-      text = event.message.text;
-      console.log("El mensaje es : " + text.substring(0, 200) + "El que lo envia es:" + sender);
-      //sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
-      sendGenericMessage(sender);
+    console.log("Llego un mensaje al webhook")
+    messaging_events = req.body.entry[0].messaging;
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i];
+        sender = event.sender.id;
+        if (event.message && event.message.text) {
+            text = event.message.text;
+            console.log("El mensaje es : " + text.substring(0, 200) + "El que lo envia es:" + sender);
+            //sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
+            sendGenericMessage(sender);
+        }
+        if (event.postback) {
+            text = JSON.stringify(event.postback);
+            sendTextMessage(sender, "Postback received: "+text.substring(0, 200));
+            continue;
+        }
     }
-  }
-  res.sendStatus(200);
+    res.sendStatus(200);
 });
 
 function sendGenericMessage(sender) {
